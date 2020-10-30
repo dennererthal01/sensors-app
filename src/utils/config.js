85411b816e3eb05576bd5f4699c0d828b6eb1b3f
dennererthal01 <dennererthal00@gmail.com>
2020-10-29 22:23:09 -0300

    removed stuff

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,11 +1,3 @@
 export default {
-    firebase: {
-        apiKey: 'AIzaSyDkAbMP4CVt5mUIYz_DS2rkjkRw5uY2Qww',
-        authDomain: 'sensors-686ea.firebaseapp.com',
-        databaseURL: 'https://sensors-686ea.firebaseio.com',
-        projectId: 'sensors-686ea',
-        storageBucket: 'sensors-686ea.appspot.com',
-        messagingSenderId: '643752702982',
-        appId: '1:643752702982:web:60cf981b66187cb7503f87',
-    },
+    firebase: {} // add config here,
 }
